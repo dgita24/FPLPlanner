afbc1d5a7939e4fc798d9d6de4694c5a1959bfe3
dgita24 <davidnash@mailbox.org>
2026-01-18 15:52:46 +0000

    update reason

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash]. js',
+        chunkFileNames: 'assets/[name].[hash]. js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
+}
